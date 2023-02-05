5befa3a53752f04cc13746e8c6c328f5e9e7be29
mehmet yıldırım <mehmetydm@gmail.com>
2023-02-06 00:16:11 +0300

    QuickUnionImprovedUF.java file added

---
+++ src/main/java/org/example/QuickUnionImprovedUF.java
@@ -0,0 +1,38 @@
+package org.example;
+
+// Weighted quick union..
+public class QuickUnionImprovedUF {
+    private int[] id;
+    private int[] sz;
+
+    public QuickUnionImprovedUF(int N) {
+        for(int i = 0; i<N; i++)
+        {
+            id[i] = i;
+            sz[i] = i;
+        }
+
+    }
+    private int root(int i){
+        while(i != id[i])
+            i = id[i];
+        return i;
+    }
+    public boolean isConnected(int p, int q){
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q){
+        int i = root(p);
+        int j = root(q);
+        if (i == j)
+            return;
+        if(sz[i] > sz[j]){
+            id[i] = j;
+            sz[j] += sz[i];
+        }else{
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+    }
+}
